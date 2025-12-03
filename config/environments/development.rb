b8cbbb1a948d7a516afe36505cc566dd25e19e62
Ibrahim Awwal <ibrahim@volition.co>
2025-12-03 13:59:52 -0800

    Assume SSL when running in Superconductor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -114,5 +114,12 @@ Rails.application.configure do
 
     # Set cookie same site protection to none to allow iframe login
     config.action_dispatch.cookies_same_site_protection = :none
+
+    # Assume all access to the app is happening through a SSL-terminating reverse proxy.
+    # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
+    config.assume_ssl = true
+
+    # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+    config.force_ssl = true
   end
 end
